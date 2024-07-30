70f517730f15f627730ec55e85196892945ddabf
Sunny Bhushan <105878283+11Bhushan11@users.noreply.github.com>
2024-07-30 07:03:21 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version 
+// alert(3+3) we are using nodejs, not browser 
+
+console.log(3+3)
+
+console.log("Hitesh")
+// use mdn webite for knowledge 
+// data types 
+/* 
+number => range 2 to power 53 after that we use bigint 
+bigint 
+string => ""
+boolean => True /False 
+null => standalone value 
+undefined => data assign nahi hua hai 
+symbol => unique 
+object 
+*/
+
+console.log(typeof "hitesh")
